0aadf5f65e8b4466281c4111b26730535b4d53d2
Aashirbad Shahi <ashahi6@myseneca.ca>
2024-05-17 12:00:36 -0400

    Added lab1b.py and lab1c.py

---
+++ lab1b.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Assigning a string to an object
+name = 'Thomas'
+
+# Printing the value of the object
+print(name)
+
+# Printing the name of the object (as a string)
+print('name')
+
+# Concatenating the string with another string
+print('I have a friend named ' + name)
+
---
+++ lab1c.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Creating two integer objects
+num1 = 5
+num2 = 10
+
+# Printing the values of the integer objects
+print(num1)
+print(num2)
+
+# Adding the values of the integer objects and storing it in a new object
+sum = num1 + num2
+
+# Printing the value of the new object
+print(sum)
+
+# Trying to concatenate a string with an integer object (this will cause an error)
+# print('The sum is: ' + sum)
+
+# Converting the integer object to a string and concatenating it with a string
+print('The sum is: ' + str(sum))
+

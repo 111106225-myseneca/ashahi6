837a8067fb1bec0f65766db5ff439652e01292dc
Aashirbad Shahi <ashahi6@myseneca.ca>
2024-05-17 13:21:30 -0400

    Added lab1c

--- lab1c.py
+++ lab1c.py
@@ -1,30 +1,12 @@
 #!/usr/bin/env python3
 
-# Creating two integer objects
-num1 = 5
-num2 = 10
-
-# Printing the values of the integer objects
-print(num1)
-print(num2)
-
-# Adding the values of the integer objects and storing it in a new object
-total_sum = num1 + num2
-
-# Printing the value of the new object
-print(total_sum)
-
-# Trying to concatenate a string with an integer object (this will cause an error)
-# print('The sum is: ' + total_sum)
-
-# Converting the integer object to a string and concatenating it with a string
-print('The sum is: ' + str(total_sum))
-
-# Defining objects for name and age
-name = 'Isaac'
+# Define name and age objects
+name = "Isaac"
 age = 72
 
-# Printing out the formatted string
-print(name + ' is ' + str(age) + ' years old!')
+# Print out the message combining name and age
+print(name + " is " + str(age) + " years old!")
+
+
 
 
